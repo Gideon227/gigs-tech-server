8b0c69f43fc4bd38801a44f9dae44583f4b7660d
Gideon <golaiya123@gmail.com>
2025-06-21 18:32:17 +0100

    fixed ecosystem

--- .ecosystem.config.js
+++ .ecosystem.config.js
@@ -1,4 +1,3 @@
-require('dotenv').config({ path: '.env.staging' });
 module.exports = {
     apps: [
       {
@@ -6,11 +5,7 @@ module.exports = {
         script: "server.js",
         cwd: "/home/ec2-user/backend",
         env: {
-          NODE_ENV: "staging",
-          PORT: 4000,
-          DATABASE_URL: process.env.DATABASE_URL,
-          REDIS_PORT: process.env.REDIS_PORT,
-          REDIS_HOST: process.env.REDIS_HOST,
+          NODE_ENV: "staging"
         },
       },
     ],
