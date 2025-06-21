b1ef0e2dc58b157bb814710898bb84ba128de15e
Gideon <golaiya123@gmail.com>
2025-06-21 18:51:47 +0100

    fixed ecosystem config code

--- .ecosystem.config.js
+++ .ecosystem.config.js
@@ -5,7 +5,13 @@ module.exports = {
         script: "server.js",
         cwd: "/home/ec2-user/backend",
         env: {
-          NODE_ENV: "staging"
+          NODE_ENV: "development",
+        },
+        env_staging: {
+          NODE_ENV: "staging",
+        },
+        env_production: {
+          NODE_ENV: "production",
         },
       },
     ],
