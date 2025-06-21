a1c73b1e9e577901218dbe0953401b55a6784d58
Gideon <golaiya123@gmail.com>
2025-06-21 17:50:22 +0100

    added environment variables in ecosystem file

--- .ecosystem.config.js
+++ .ecosystem.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: '.env.staging' });
 module.exports = {
     apps: [
       {
@@ -6,8 +7,12 @@ module.exports = {
         cwd: "/home/ec2-user/backend",
         env: {
           NODE_ENV: "staging",
+          PORT: 4000,
+          DATABASE_URL: process.env.DATABASE_URL,
+          REDIS_PORT: process.env.REDIS_PORT,
+          REDIS_HOST: process.env.REDIS_HOST,
         },
       },
     ],
-  };
+};
   
