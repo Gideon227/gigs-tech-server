4cada5981adf85c356e91ffd1500c9f84a268158
Gideon <golaiya123@gmail.com>
2025-11-17 16:24:47 +0100

    minor changes

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,31 +1,30 @@
 const asyncHandler = require('../middleware/asyncHandler');
-const contactService = require('../services/contact.service')
+const contactService = require('../services/contact.service');
 
-exports.handleContactForm = asyncHandler(async (req, res, next) => {
+exports.handleContactForm = asyncHandler(async (req, res) => {
+  const { email, message } = req.body;
+
+  if (!email || !message) {
+    return res.status(400).json({ error: "Email and message are required" });
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ 
+      success: false, 
+      message: "Invalid email address." 
+    });
+  }
+
   try {
-    const { email, message } = req.body;
-
-    if (!email || !message) {
-      return res.status(400).json({ error: "Email and message are required" });
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({ 
-        success: false, 
-        message: "Invalid email address." 
-      });
-    }
-
     await contactService.sendContactEmail({ email, message });
-
     return res.status(200).json({ message: "Message sent successfully!" });
   } catch (error) {
     console.error("Contact route error:", error);
-    res.status(500).json({ 
+    
+    return res.status(500).json({ 
       success: false, 
       message: "Failed to send message. Please try again later." 
     });
-    next(error);
   }
-})
+});
